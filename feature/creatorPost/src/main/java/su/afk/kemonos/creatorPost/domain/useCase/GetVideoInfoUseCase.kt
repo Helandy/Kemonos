bf90e48bfe3d03fbc24fffee699168b8619d6987
Helandy <3n3my3@gmail.com>
2026-01-01 20:35:19 +0300

    deleted unnecessary anotations

--- feature/creatorPost/src/main/java/su/afk/kemonos/creatorPost/domain/useCase/GetVideoInfoUseCase.kt
+++ feature/creatorPost/src/main/java/su/afk/kemonos/creatorPost/domain/useCase/GetVideoInfoUseCase.kt
@@ -1,8 +1,6 @@
 package su.afk.kemonos.creatorPost.domain.useCase
 
 import android.media.MediaMetadataRetriever
-import android.os.Build
-import androidx.annotation.RequiresApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
@@ -22,7 +20,6 @@ class GetVideoInfoUseCase @Inject constructor(
     @Named("VideoInfoClient") private val http: OkHttpClient,
     private val cache: IVideoInfoUseCase
 ) {
-    @RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     suspend operator fun invoke(url: String, name: String): VideoInfo = withContext(Dispatchers.IO) {
         val cached = cache.get(name)
         if (cached != null) return@withContext cached
