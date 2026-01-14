5dc5f412b08e0bbd472d32cdd4831ffca8e82b73
Helandy <3n3my3@gmail.com>
2026-01-14 18:20:46 +0300

    fix color text to android version < 12

--- app/src/main/java/su/afk/kemonos/ui/theme/Theme.kt
+++ app/src/main/java/su/afk/kemonos/ui/theme/Theme.kt
@@ -2,28 +2,14 @@ package su.afk.kemonos.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-    background = Color(29, 31, 32),
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    background = Color(29, 31, 32),
-
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
