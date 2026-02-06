0d4e75be5ce7048a51da29a7410657ddc0e8ab1b
Helandy <3n3my3@gmail.com>
2026-02-06 17:32:40 +0300

    hide http logging in release

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -12,6 +12,9 @@ android {
     defaultConfig {
         minSdk = libs.versions.minSdk.get().toInt()
     }
+    buildFeatures {
+        buildConfig = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
