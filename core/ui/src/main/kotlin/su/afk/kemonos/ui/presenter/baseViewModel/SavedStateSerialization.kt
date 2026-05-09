0d812b6d2471ae93fc115ee0e63421e71980999d
Helandy <3n3my3@gmail.com>
2026-05-09 09:49:24 +0300

    fix warning

--- core/ui/src/main/kotlin/su/afk/kemonos/ui/presenter/baseViewModel/SavedStateSerialization.kt
+++ core/ui/src/main/kotlin/su/afk/kemonos/ui/presenter/baseViewModel/SavedStateSerialization.kt
@@ -5,12 +5,12 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.savedstate.serialization.decodeFromSavedState
 import androidx.savedstate.serialization.encodeToSavedState
 
-inline fun <reified T> SavedStateHandle.getSerializableState(key: String): T? {
+inline fun <reified T : Any> SavedStateHandle.getSerializableState(key: String): T? {
     val savedState = get<Bundle>(key) ?: return null
     return runCatching { decodeFromSavedState<T>(savedState) }.getOrNull()
 }
 
-inline fun <reified T> SavedStateHandle.setSerializableState(key: String, value: T?) {
+inline fun <reified T : Any> SavedStateHandle.setSerializableState(key: String, value: T?) {
     if (value == null) {
         remove<Bundle>(key)
     } else {
